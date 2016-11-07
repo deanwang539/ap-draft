0dc5cdafcbfd48c0c85ea4538d503d04801a64ee
Dean Wong <Dean Wong>
2016-11-06 19:14:46 -0500

    master: added kakao exp-menu button effects for

--- www/states/warmup/warmup.js
+++ www/states/warmup/warmup.js
@@ -16,10 +16,25 @@ $(document).ready(function(){
     $("#exp-menu input").click(function(){
       if($("#exp-menu input").is(':checked')){
         var pChart = $('#aChart').offset();
+        var pTop = pChart.top;
+        var pLeft = pChart.left-$('#aChart').width();
         $('#dChart').append('Chart');
-        $('#dChart').css({"position": "absolute", 'top':pChart.top-$('#aChart').height()*1.1, 'left':pChart.left-$('#aChart').width()});
+        $('#dChart').css({"position": "absolute", 'top':pTop-$('#aChart').height()*1.15, 'left':pLeft});
+        $('#dPlus').append('Plus');
+        $('#dPlus').css({"position": "absolute", 'top':pTop-$('#aChart').height()*2.55, 'left':pLeft});
+        $('#dHeart').append('Heart');
+        $('#dHeart').css({"position": "absolute", 'top':pTop-$('#aChart').height()*3.95, 'left':pLeft});
+        $('#dEnvelope').append('Enve');
+        $('#dEnvelope').css({"position": "absolute", 'top':pTop-$('#aChart').height()*5.35, 'left':pLeft});
+        $(".title-item").fadeIn(500);
+        $(".menu-clicked").show();
       }else{
         $('#dChart').html("");
+        $('#dPlus').html("");
+        $('#dHeart').html("");
+        $('#dEnvelope').html("");
+        $(".title-item").hide();
+        $(".menu-clicked").hide();
       }
     });
 });
