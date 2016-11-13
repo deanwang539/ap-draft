3a6cceda2f603fbdbb1ede006c29fdfb3196edf2
Dean Wong <Dean Wong>
2016-11-12 20:47:56 -0500

    master: added tutorial carousel

--- www/states/tutorial/tutorial.js
+++ www/states/tutorial/tutorial.js
@@ -1,2 +1,6 @@
 $(document).ready(function() {
+  $("#owl-content").owlCarousel();
+  var contentH = $(".content").height();
+  var owlPageH = $(".owl-theme .owl-controls").height();
+  $(".owl-carousel .owl-wrapper-outer").height(contentH - owlPageH*2);
 });
