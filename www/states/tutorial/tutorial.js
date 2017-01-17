7f5fae2407646842f0ed6a03668a61c0e0b95575
Dean Wong <Dean Wong>
2017-01-17 17:58:54 -0500

    master: fixed tutorial btn delay

--- www/states/tutorial/tutorial.js
+++ www/states/tutorial/tutorial.js
@@ -37,24 +37,40 @@ var app = {
 
         // $('#continue').delayed('click', 300, function() {
         $('#continue').on('click', function() {
-          // page transitions
-          window.plugins.nativepagetransitions.slide({
-            "direction"        : "down", // 'left|right|up|down', default 'left' (which is like 'next')
-            "duration"         :  300, // in milliseconds (ms), default 400
-            "slowdownfactor"   :    3, // overlap views (higher number is more) or no overlap (1). -1 doesn't slide at all. Default 4
-            "slidePixels"      :  100, // optional, works nice with slowdownfactor -1 to create a 'material design'-like effect. Default not set so it slides the entire page.
-            "androiddelay"     :   -1, // same as above but for Android, default 70
-            "href" : "../ap/ap.html"
+          $("#continue_btn").css({
+            "transition-timing-function": "linear",
+            "transition-duration": "200ms",
+            "-webkit-transform": "scale(0.9, 0.9) translate3d(0, 0 , 0)",
+            "opacity": "0.8"
           });
+          setTimeout(function(){
+            // page transitions
+            window.plugins.nativepagetransitions.slide({
+              "direction"        : "down", // 'left|right|up|down', default 'left' (which is like 'next')
+              "duration"         :  300, // in milliseconds (ms), default 400
+              "slowdownfactor"   :    3, // overlap views (higher number is more) or no overlap (1). -1 doesn't slide at all. Default 4
+              "slidePixels"      :  100, // optional, works nice with slowdownfactor -1 to create a 'material design'-like effect. Default not set so it slides the entire page.
+              "androiddelay"     :   -1, // same as above but for Android, default 70
+              "href" : "../ap/ap.html"
+            });
+          }, 300);
         });
 
         $('#watch_video').delayed('click', 300, function() {
           // VideoPlayer.play("http://um3dlab-db.miserver.it.umich.edu/acupressure/acupressure-test.mp4");
           var options = {
             successCallback: function() {
+              $("#watch_video_btn").css({
+                "-webkit-transform": "scale(1.0, 1.0) translate3d(0, 0 , 0)",
+                "opacity": "1.0"
+              });
               console.log("Video was closed without error.");
             },
             errorCallback: function(errMsg) {
+              $("#watch_video_btn").css({
+                "-webkit-transform": "scale(1.0, 1.0) translate3d(0, 0 , 0)",
+                "opacity": "1.0"
+              });
               console.log("Error! " + errMsg);
             },
             // orientation: 'landscape',
